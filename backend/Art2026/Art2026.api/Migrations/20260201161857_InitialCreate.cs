8afac5303669fa3a046af721d59d38e996aec694
katarinamarj <katarinamarjanovic02@gmail.com>
2026-02-01 19:01:28 +0100

    Add initial database migration

---
+++ backend/Art2026/Art2026.api/Migrations/20260201161857_InitialCreate.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Art2026.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Manifestations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    City = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Location = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    StartDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    AdditionalInfo = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    MaxVisitorsPerDay = table.Column<int>(type: "int", nullable: false),
+                    PricePainting = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    PricePhotography = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    DiscountEndDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manifestations", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ManifestationDays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DayType = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ManifestationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManifestationDays", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ManifestationDays_Manifestations_ManifestationId",
+                        column: x => x.ManifestationId,
+                        principalTable: "Manifestations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Exhibitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    OpensAt = table.Column<TimeSpan>(type: "time(6)", nullable: false),
+                    ClosesAt = table.Column<TimeSpan>(type: "time(6)", nullable: false),
+                    Artists = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ManifestationDayId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exhibitions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Exhibitions_ManifestationDays_ManifestationDayId",
+                        column: x => x.ManifestationDayId,
+                        principalTable: "ManifestationDays",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PromoCodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Code = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RegistrationId = table.Column<int>(type: "int", nullable: false),
+                    UsedByRegistrationId = table.Column<int>(type: "int", nullable: true),
+                    UsedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    InvalidatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PromoCodes", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Profession = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Address1 = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Address2 = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PostalCode = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Place = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Country = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EmailConfirmed = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    NumberOfPersons = table.Column<int>(type: "int", nullable: false),
+                    AccessToken = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UsedPromoCodeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Registrations_PromoCodes_UsedPromoCodeId",
+                        column: x => x.UsedPromoCodeId,
+                        principalTable: "PromoCodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "RegistrationDays",
+                columns: table => new
+                {
+                    RegistrationId = table.Column<int>(type: "int", nullable: false),
+                    ManifestationDayId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistrationDays", x => new { x.RegistrationId, x.ManifestationDayId });
+                    table.ForeignKey(
+                        name: "FK_RegistrationDays_ManifestationDays_ManifestationDayId",
+                        column: x => x.ManifestationDayId,
+                        principalTable: "ManifestationDays",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RegistrationDays_Registrations_RegistrationId",
+                        column: x => x.RegistrationId,
+                        principalTable: "Registrations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exhibitions_ManifestationDayId",
+                table: "Exhibitions",
+                column: "ManifestationDayId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ManifestationDays_ManifestationId",
+                table: "ManifestationDays",
+                column: "ManifestationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PromoCodes_Code",
+                table: "PromoCodes",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PromoCodes_RegistrationId",
+                table: "PromoCodes",
+                column: "RegistrationId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PromoCodes_UsedByRegistrationId",
+                table: "PromoCodes",
+                column: "UsedByRegistrationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RegistrationDays_ManifestationDayId",
+                table: "RegistrationDays",
+                column: "ManifestationDayId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_AccessToken",
+                table: "Registrations",
+                column: "AccessToken",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_UsedPromoCodeId",
+                table: "Registrations",
+                column: "UsedPromoCodeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PromoCodes_Registrations_RegistrationId",
+                table: "PromoCodes",
+                column: "RegistrationId",
+                principalTable: "Registrations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PromoCodes_Registrations_UsedByRegistrationId",
+                table: "PromoCodes",
+                column: "UsedByRegistrationId",
+                principalTable: "Registrations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PromoCodes_Registrations_RegistrationId",
+                table: "PromoCodes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PromoCodes_Registrations_UsedByRegistrationId",
+                table: "PromoCodes");
+
+            migrationBuilder.DropTable(
+                name: "Exhibitions");
+
+            migrationBuilder.DropTable(
+                name: "RegistrationDays");
+
+            migrationBuilder.DropTable(
+                name: "ManifestationDays");
+
+            migrationBuilder.DropTable(
+                name: "Manifestations");
+
+            migrationBuilder.DropTable(
+                name: "Registrations");
+
+            migrationBuilder.DropTable(
+                name: "PromoCodes");
+        }
+    }
+}
